add4e83f2afe1e59d2b6bcada84c9c29d594a5f5
nsankethreddy <sankethn1947@gmail.com>
2020-07-13 20:05:31 +0530

    add ranking and ordering

--- RankMoviesOrder.py
+++ RankMoviesOrder.py
@@ -4,7 +4,7 @@ from mrjob.step import MRStep
 
 class RankMovies(MRJob):
     def steps(self):
-        return[MRStep(mapper=self.mapper_get_movieIDs, reducer=self.reducer_count_movieIDs)]
+        return[MRStep(mapper=self.mapper_get_movieIDs, reducer=self.reducer_count_movieIDs), MRStep(reducer=self.reducer_sort)]
 
     def mapper_get_movieIDs(self, _, line):
         (userID, movieID, rating, timestamp) = line.split('\t')
@@ -13,6 +13,8 @@ class RankMovies(MRJob):
     def reducer_count_movieIDs(self, key, values):
         yield key, sum(values)
 
+    def reducer_sort(self, key, values):
+        yield values, key
 
 if __name__ == '__main__':
     RankMovies.run()
--- RatingsBreakdown.py
+++ RatingsBreakdown.py
@@ -6,11 +6,11 @@ class RatingsBreakdown(MRJob):
         return[ MRStep(mapper=self.mapper_get_ratings, reducer=self.reducer_count_ratings)] 
 
     def mapper_get_ratings(self, _, line):
-    (userID, movieID, rating, timestamp) = line.split('\t')
-    yield rating, 1
+        (userID, movieID, rating, timestamp) = line.split('\t')
+        yield rating, 1
 
     def reducer_count_ratings(self, key, values):
-    yield key, sum(values)
+        yield key, sum(values)
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     RatingsBreakdown.run()
